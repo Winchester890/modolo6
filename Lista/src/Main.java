23d664af4d7d1207d7a18189d63cb4cc631b293b
Winchester890 <126791337+Winchester890@users.noreply.github.com>
2023-03-24 23:42:20 -0300

    Update Main.java

--- Lista/src/Main.java
+++ Lista/src/Main.java
@@ -1,41 +1,57 @@
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
-import Pacote.Aluno;
-import Pacote.Presenca;
+import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<String> nomes = new ArrayList<>();
-        List<String> dias = new ArrayList<>();
-        List<String> presentes = new ArrayList<>();
-        List<String> ausentes = new ArrayList<>();
-        System.out.print("Quantos alunos serão digitados? ");
+        System.out.print("Quantos alunos serão contabilizados? ");
         int N = Integer.parseInt(scanner.next());
+        List<String> nomes = new ArrayList<>();
         for (int i = 0; i < N; i++) {
-            System.out.print("Digite o nome do(a) aluno(a): ");
-            String nome = String.join(scanner.next());
+            System.out.print("Digite o nome do aluno: ");
+            String nome = scanner.next();
             nomes.add(nome);
         }
-        System.out.print("Quantos dias serão contabilizados? ");
-        N = Integer.parseInt(scanner.next());
+        HashMap<Integer, String> meses = new HashMap<>();
+        meses.put(2, "Fevereiro");
+        meses.put(3, "Março");
+        meses.put(4, "Abril");
+        meses.put(5, "Maio");
+        meses.put(6, "Junho");
+        meses.put(8, "Agosto");
+        meses.put(9, "Setembro");
+        meses.put(10, "Outubro");
+        meses.put(11, "Novembro");
+
+        int mes;
+        System.out.print("Indique o mês (pelo n°) para contabilizar as faltas: ");
+        do {
+            mes = Integer.parseInt(scanner.next());
+            if (mes == 1 || mes == 7 || mes == 12) {
+                System.out.print("Mês contabilizado como período de férias. Selecione outro: ");
+            }
+        } while (mes == 1 || mes == 7 || mes == 12);
+
+        int contFaltas = 0;
+        List<Integer> faltas = new ArrayList<>();
+        System.out.println("Digite quantas faltas cada aluno teve em ordem");
+        System.out.print("Assim: na primeira linha você irá digitar as faltas do primeiro aluno contabilizado: ");
+        int falta;
         for (int i = 0; i < N; i++) {
-            System.out.print("Digite o dia: ");
-            String dia = String.join(scanner.next());
-            dias.add(dia);
-            System.out.print("Qual(is) aluno(s) estive(ram) presente(s) neste dia? ");
-            String presente = String.join(scanner.next());
-            presentes.add(presente);
-            System.out.print("Qual(is) aluno(s) estive(ram) ausente(s) neste dia? ");
-            String ausente = String.join(scanner.next());
-            ausentes.add(ausente);
+            falta = Integer.parseInt(scanner.next());
+            faltas.add(falta);
+            contFaltas = contFaltas + faltas.get(i);
         }
-        System.out.println("---- Diário de classe ----");
-        System.out.println("Sala: " + nomes);
+        System.out.println("----- Diário de classe no mês de " + meses.get(mes) + " -----");
+        System.out.println("Alunos: " + nomes);
+        System.out.println("Faltas de cada aluno:");
         for (int i = 0; i < N; i++) {
-            System.out.println("Dia " + dias.get(i) + ":");
-            System.out.println("Alunos presentes: " + presentes.get(i));
-            System.out.println("Alunos ausentes: " + ausentes.get(i));
+            System.out.println(nomes.get(i) + ": " + faltas.get(i));
         }
+        System.out.println(" ");
+        System.out.println("Faltas totais: " + contFaltas);
+        float percFaltas = (contFaltas * N) / 20;
+        System.out.println("Percentual de faltas: " + percFaltas + "%");
     }
 }
